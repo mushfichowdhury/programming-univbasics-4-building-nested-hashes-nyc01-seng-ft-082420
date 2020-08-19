025534c50cc3e7d930ae3af796f70ce87f5b1ac1
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-19 08:14:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,9 @@ def monopoly_with_third_tier
     hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
+      :rent_in_dollars => {
+        
+      },
       :names => {}
     }
   }
