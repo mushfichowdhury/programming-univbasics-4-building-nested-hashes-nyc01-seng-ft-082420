d10eeebd40c09e79215821a6bc675668fe94d6e0
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-19 08:13:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
   hash = {
-    :railrods => {}
+    :railroads => {}
   }
   hash 
   # Remember implicit return! We're returning this new Hash _without_ the
@@ -10,7 +10,7 @@ end
 
 def monopoly_with_second_tier
   hash = {
-    :railrods => {
+    :railroads => {
       :pieces => 4,
       :rent_in_dollars => {},
       :names => {}
